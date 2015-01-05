590b94d6cea762306f1932494fae85b235c5354b
Chris Olstrom <chris@olstrom.com>
2015-01-05 00:29:26 -0800

    Trivial stylistic changes.

--- optimize-images.rb
+++ optimize-images.rb
@@ -7,11 +7,11 @@ optipng = `which optipng`
 jpegtran = `which jpegtran`
 gifsicle = `which gifsicle`
 
-png = ".png"
-jpg = ".jpg"
-gif = ".gif"
+png = '.png'
+jpg = '.jpg'
+gif = '.gif'
 
-suffix = "-optimized"
+suffix = '-optimized'
 replace_original = false
 
 supported_formats = []
@@ -24,21 +24,21 @@ supported_formats.each do |format|
 
   case format
   when png
-    optimize = [optipng, "PH_FILE", "-out PH_OUTPUT_FILE"]
+    optimize = [optipng, 'PH_FILE', '-out PH_OUTPUT_FILE']
   when jpg
-    optimize = [jpegtran, "-optimize PH_FILE", "-optimize -outfile PH_OUTPUT_FILE PH_FILE"]
+    optimize = [jpegtran, '-optimize PH_FILE', '-optimize -outfile PH_OUTPUT_FILE PH_FILE']
   when gif
-    optimize = [gifsicle, "--optimize PH_FILE", "--optimize --output=PH_OUTPUT_FILE PH_FILE"]
+    optimize = [gifsicle, '--optimize PH_FILE', '--optimize --output=PH_OUTPUT_FILE PH_FILE']
   end
 
   Dir.glob(dir + "/**/*#{format}", File::FNM_CASEFOLD).each do |file|
-    output_file = "#{File.dirname(file)}/#{File.basename(file,File.extname(file))}#{suffix}#{format}"
+    output_file = "#{ File.dirname(file) }/#{ File.basename(file, File.extname(file)) }#{ suffix }#{ format }"
     if replace_original
-      p "- #{file}"
-      `#{optimize[0]} #{optimize[1].sub('PH_FILE', file).sub('PH_OUTPUT_FILE', output_file)}`
+      p "- #{ file }"
+      `#{ optimize[0] } #{ optimize[1].sub('PH_FILE', file).sub('PH_OUTPUT_FILE', output_file) }`
     else
-      p "- #{file} > #{output_file}"
-      `#{optimize[0]} #{optimize[2].sub('PH_FILE', file).sub('PH_OUTPUT_FILE', output_file)}`
+      p "- #{ file } > #{ output_file }"
+      `#{ optimize[0] } #{ optimize[2].sub('PH_FILE', file).sub('PH_OUTPUT_FILE', output_file) }`
     end
   end
 end
